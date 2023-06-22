6e0a14fd64a3dfcce2d306f932a5adced409e72c
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-06-21 18:20:51 -0700

    Set up importing for players and update Schema

--- server/controllers/NBAPlayerImport.js
+++ server/controllers/NBAPlayerImport.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises');
 const Player = require('../models/Player');
+const scraper = require('nba-stat-scraper');
 
 // USE INSTRUCTIONS: Update file contents and year stats are saving to
 async function importNBARegularSeasonData(req, res, next) {
@@ -12,30 +13,19 @@ async function importNBARegularSeasonData(req, res, next) {
   const fileLines = fileContents.split(/\r?\n/);
 
   // For each line, split it by commas then put it into the DB based off the stats
-  for (let i = 0; i < fileLines.length - 1; i++) {
-    const line = fileLines[i];
-    console.log(line);
+  for (let i = 0; i < 3; i++) {
+    const playerName = fileLines[i].split(',')[1];
 
-    // const newPlayer = new Player({
-    //   league: 'NBA',
-    //   name: { type: String, required: true, minLength: 1, maxLength: 100 },
-    //   picture: { type: String, required: true, maxLength: 100 },
-    //   stats: { type: Object, required: true },
-    //   funFacts: { type: Array, required: true },
-    //   nicknames: { type: Array, required: true },
-    //   championships: { type: Number, required: true },
-    //   college: { type: String, required: true },
-    //   pick: { type: Number, required: true },
-    //   place_of_birth: { type: String, required: true, maxLength: 50 },
-    //   birth_date: { type: Date, required: true },
-    //   first_year: { type: Number, required: true },
-    //   last_year: { type: Number, required: true },
-    // });
+    const player = await scraper.getPlayer(playerName);
 
-    // await newPlayer.save();
+    const playerObject = JSON.parse(player);
+
+    const newPlayer = new Player(playerObject);
+
+    await newPlayer.save();
   }
 
   res.send('dasdasdas');
 }
 
-module.exports = importNBARegulatSeasonData;
+module.exports = importNBARegularSeasonData;
--- server/models/Player.js
+++ server/models/Player.js
@@ -2,20 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const NBAPlayerSchema = new Schema({
-  league: { type: String, required: true, minLength: 1, maxLength: 5 },
   name: { type: String, required: true, minLength: 1, maxLength: 100 },
   picture: { type: String, required: true, maxLength: 100 },
-  stats: { type: Object, required: true },
-  funFacts: { type: Array, required: true },
+  positions: { type: Array, required: true },
+  height: { type: String, required: true, minLength: 1, maxLength: 5 },
+  weight: { type: Number, required: true },
   nicknames: { type: Array, required: true },
   championships: { type: Number, required: true },
-  college: { type: String, required: true },
-  pick: { type: Number, required: true },
-  place_of_birth: { type: String, required: true, maxLength: 50 },
-  birth_date: { type: Date, required: true },
-  first_year: { type: Number, required: true },
-  last_year: { type: Number, required: true },
-  playerId: { type: Number },
+  mvps: { type: Number, required: true },
+  allStars: { type: Number, required: true },
+  accolades: { type: Array, required: true },
+  stats: { type: Object, required: true },
+  funFacts: { type: Array, required: true },
+  shootingHand: { type: Array, required: true },
+  college: { type: String, required: false },
+  birthplace: { type: String, required: true, maxLength: 50 },
+  birthdate: { type: Date, required: true },
+  draftPick: { type: Number, required: true },
+  draftYear: { type: Number, required: true },
+  draftTeam: { type: String, required: false, minLength: 1, maxLength: 50 },
+  debut: { type: Date, required: false },
 });
 
 module.exports = mongoose.model('NBAPlayer', NBAPlayerSchema);
