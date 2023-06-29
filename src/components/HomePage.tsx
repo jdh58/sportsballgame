d43005234fa0121857d1ef0be4a53db88d1d93c2
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-06-29 08:54:30 -0700

    Style Tile buttons and images

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import TileButton from './TileButton';
 import Footer from './Footer';
+import Curry from '../assets/curry.jpg';
 import '../styles/HomePage.css';
 
 export default function HomePage() {
@@ -19,25 +20,27 @@ export default function HomePage() {
   return (
     <>
       <main className="page homePage">
-        {isLoggedIn ? (
-          <h2 className="welcome">Welcome, {User.username}</h2>
-        ) : (
-          <h2 className="welcome">
-            You're currently signed out. <Link to="/signin">Sign in</Link> or{' '}
-            <Link to="/signup">Sign up</Link>?
-          </h2>
-        )}
-        <div className="optionsContainer">
-          <div className="left">
-            <TileButton label="Who Am I?" bgImage={null} />
-            <TileButton label="Higher or Lower" bgImage={null} />
-            <TileButton label="More" bgImage={null} />
-          </div>
-          <div className="separator"></div>
-          <div className="right">
-            <TileButton label="Profile" bgImage={null} />
-            <TileButton label="Leaderboards" bgImage={null} />
-            <TileButton label="Settings" bgImage={null} />
+        <div className="mainContainer">
+          {isLoggedIn ? (
+            <h2 className="welcome">Welcome, {User.username}</h2>
+          ) : (
+            <h2 className="welcome">
+              You're currently signed out. <Link to="/signin">Sign in</Link> or{' '}
+              <Link to="/signup">Sign up</Link>?
+            </h2>
+          )}
+          <div className="optionsContainer">
+            <div className="left">
+              <TileButton label="Who Am I?" bgImage={null} />
+              <TileButton label="Higher or Lower" bgImage={Curry} />
+              <TileButton label="More" bgImage={null} />
+            </div>
+            <div className="separator"></div>
+            <div className="right">
+              <TileButton label="Profile" bgImage={null} />
+              <TileButton label="Leaderboards" bgImage={null} />
+              <TileButton label="Settings" bgImage={null} />
+            </div>
           </div>
         </div>
       </main>
--- src/components/TileButton.tsx
+++ src/components/TileButton.tsx
@@ -1,4 +1,5 @@
 import '../styles/TileButton.css';
+import Curry from '../assets/curry.jpg';
 
 export default function TileButton({
   label,
@@ -8,8 +9,31 @@ export default function TileButton({
   bgImage: string | null;
 }) {
   return (
-    <div className="tileButton">
-      <p className="label">{label}</p>
+    <div
+      className="tileButtonContainer"
+      style={
+        bgImage
+          ? {
+              backgroundImage: `url(${Curry})`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+              backgroundSize: 'cover',
+            }
+          : {}
+      }
+    >
+      <div
+        className={bgImage ? 'tileButton booo' : 'tileButton'}
+        style={
+          bgImage
+            ? {
+                backgroundImage: `linear-gradient(0deg, rgba(20, 2, 2, .75), rgba(20, 20, 2, .2))`,
+              }
+            : {}
+        }
+      >
+        <p className="label">{label}</p>
+      </div>
     </div>
   );
 }
