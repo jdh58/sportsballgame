659a8b6e132d2f6ff09867b5f5a200505960ae1b
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-07-11 07:21:36 -0700

    Hammer out basic CSS outline for the profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -31,19 +31,34 @@ export default function Profile() {
           </h2>
           <div className="scoresContainer">
             <div className="third">
-              <img src="ribbon" alt="" />
-              <h3 className="score">2</h3>
-              <p className="topPercentage">Top 32.6%</p>
+              <div className="ribbonContainer">
+                <img src="ribbon" alt="" />
+              </div>
+              <div className="scoreInfo">
+                <h3 className="score">2</h3>
+                <p className="topPercentage">Top 32.6%</p>
+                <p className="game">NFL Who Am I (5 Rds)</p>
+              </div>
             </div>
             <div className="first">
-              <img src="ribbon" alt="" />
-              <h3 className="score">32</h3>
-              <p className="topPercentage">Top 1.1%</p>
+              <div className="ribbonContainer">
+                <img src="ribbon" alt="" />
+              </div>
+              <div className="scoreInfo">
+                <h3 className="score">32</h3>
+                <p className="topPercentage">Top 1.1%</p>
+                <p className="game">NBA Who Am I (3 Min)</p>
+              </div>
             </div>
             <div className="second">
-              <img src="ribbon" alt="" />
-              <h3 className="score">4</h3>
-              <p className="topPercentage">Top 2.7%</p>
+              <div className="ribbonContainer">
+                <img src="ribbon" alt="" />
+              </div>
+              <div className="scoreInfo">
+                <h3 className="score">4</h3>
+                <p className="topPercentage">Top 2.7%</p>
+                <p className="game">MLB Grids (3 Rds)</p>
+              </div>
             </div>
           </div>
         </div>
@@ -54,22 +69,22 @@ export default function Profile() {
             <div className="team">
               <h3>Team:</h3>
               <img src="team" alt="" />
-              <p>Top 32.6%</p>
+              <p>Portland Trail Blazers</p>
             </div>
             <div className="sport">
               <h3>Sport:</h3>
               <img src="sportsball" alt="" />
-              <p>Top 1.1%</p>
+              <p>Baseball</p>
             </div>
             <div className="game">
               <h3>Game:</h3>
               <img src="gameIcon" alt="" />
-              <p>Top 2.7%</p>
+              <p>Who Am I</p>
             </div>
             <div className="player">
               <h3>Player:</h3>
               <img src="playerHeadshot" alt="" />
-              <p>Top 2.7%</p>
+              <p>Aaron Rodgers</p>
             </div>
           </div>
         </div>
