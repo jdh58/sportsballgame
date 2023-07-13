355986bb92868b3243f302bb2ceea1e34e937dc9
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-07-13 09:56:45 -0700

    Style the sign up form

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import Button from '../components/Button';
 
-import '../styles/TextInput.css';
 import '../styles/Signup.css';
 
 export default function Signup() {
@@ -12,19 +11,30 @@ export default function Signup() {
   return (
     <main className="page signupPage">
       <div className="mainContainer">
-        <h1>Log in</h1>
+        <h1>Sign up</h1>
         <form action="" className="signupForm" onSubmit={handleFormSubmit}>
           <div className="inputContainer">
-            <input type="email" />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" />
           </div>
           <div className="inputContainer">
-            <input type="text" />
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" />
           </div>
           <div className="inputContainer">
-            <input type="password" />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
           </div>
+          <ul className="errors">
+            <li>Please enter the proper thing</li>
+          </ul>
           <div className="buttonContainer">
-            <Button />
+            <Button
+              label="Create Account"
+              type="submit"
+              icon=""
+              classes="login"
+            />
             <p>
               Already have an account? <Link to="/login">Log in</Link>
             </p>
