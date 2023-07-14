b764f1bbe81e2e1042e626f1220ae5b04c837f7d
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-07-14 10:48:43 -0700

    Set up schema for WhoAmI game

--- backend/models/WhoAmI.js
+++ backend/models/WhoAmI.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const WhoAmISchema = new Schema({
-  userID: { type: Object },
-});
+const WhoAmISchema = new Schema(
+  {
+    userID: { type: mongoose.SchemaTypes.ObjectId, required: true },
+    gameMode: { type: Object, required: true },
+    hints: { type: Object, required: true },
+    currentRound: { type: Number, required: true },
+    currentHint: { type: Number, required: true },
+    score: { type: Number, required: true },
+    correctPlayer: { type: mongoose.SchemaTypes.ObjectId, required: true },
+  },
+  { timestamps: true }
+);
