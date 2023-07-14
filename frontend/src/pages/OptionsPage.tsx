735df59884d0570e48dfc7a255f0edbde8329bb5
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-07-13 20:19:30 -0700

    Get rid of option containers and set up state

--- frontend/src/pages/OptionsPage.tsx
+++ frontend/src/pages/OptionsPage.tsx
@@ -1,13 +1,22 @@
-import SportsContainer from '../components/SportsContainer';
-import DifficultyContainer from '../components/DifficultyContainer';
-import RoundContainer from '../components/RoundContainer';
 import ToggleSwitch from '../components/ToggleSwitch';
 import Button from '../components/Button';
+import NBA from '../assets/nba.png';
+import MLB from '../assets/mlb.png';
+import NFL from '../assets/nfl.png';
+
+import '../styles/SportsContainer.css';
+import '../styles/DifficultyContainer.css';
+import '../styles/RoundContainer.css';
 
 import ArrowRight from '../assets/arrow-right.svg';
 import '../styles/OptionsPage.scss';
+import { useState } from 'react';
 
 export default function OptionsPage() {
+  const [sport, setSport] = useState('nba');
+  const [difficulty, setDifficulty] = useState('medium');
+  const [rounds, setRounds] = useState('free');
+
   return (
     <div className="page optionsPage">
       <div className="mainContainer">
@@ -21,19 +30,146 @@ export default function OptionsPage() {
         </h2>
         <div className="optionContainer">
           <h3>Sport</h3>
-          <SportsContainer />
+          <div className="sportsContainer">
+            <button
+              className={`optionButton ${sport === 'nba' ? 'selected' : ''}`}
+              onClick={() => {
+                setSport('nba');
+              }}
+            >
+              <div className="optionPicContainer">
+                <img src={NBA} alt="" />
+              </div>
+              <p>NBA</p>
+            </button>
+            <button
+              className={`optionButton ${sport === 'nfl' ? 'selected' : ''}`}
+              onClick={() => {
+                setSport('nfl');
+              }}
+            >
+              <div className="optionPicContainer">
+                <img src={NFL} alt="" />
+              </div>
+              <p>NFL</p>
+            </button>
+            <button
+              className={`optionButton ${sport === 'mlb' ? 'selected' : ''}`}
+              onClick={() => {
+                setSport('mlb');
+              }}
+            >
+              <div className="optionPicContainer">
+                <img src={MLB} alt="" />
+              </div>
+              <p>MLB</p>
+            </button>
+          </div>
         </div>
         <div className="optionContainer">
           <h3>Difficulty</h3>
-          <DifficultyContainer sport="nba" />
+          <div className="difficultyContainer">
+            <button
+              className={`optionButton ${
+                difficulty === 'easy' ? 'selected' : ''
+              }`}
+              onClick={() => {
+                setDifficulty('easy');
+              }}
+            >
+              <p className="label">35+ Mins/G</p>
+              <p className="easy">Easy</p>
+            </button>
+            <button
+              className={`optionButton ${
+                difficulty === 'medium' ? 'selected' : ''
+              }`}
+              onClick={() => {
+                setDifficulty('medium');
+              }}
+            >
+              <p className="label">15+ Mins/G</p>
+              <p className="medium">Medium</p>
+            </button>
+            <button
+              className={`optionButton ${
+                difficulty === 'hard' ? 'selected' : ''
+              }`}
+              onClick={() => {
+                setDifficulty('hard');
+              }}
+            >
+              <p className="label">All Players</p>
+              <p className="hard">Hard</p>
+            </button>
+          </div>
         </div>
         <div className="optionContainer">
           <div className="toggle">
             <h3 className="rounds">Rounds</h3>
-            <ToggleSwitch />
+            <ToggleSwitch active={false} />
             <h3 className="time">Time</h3>
           </div>
-          <RoundContainer />
+          <div className="roundContainer">
+            <button
+              className={`optionButton ${rounds === 'free' ? 'selected' : ''}`}
+              onClick={() => {
+                setRounds('free');
+              }}
+            >
+              <p className="label">Freeplay</p>
+            </button>
+            <button
+              className={`optionButton ${rounds === 'three' ? 'selected' : ''}`}
+              onClick={() => {
+                setRounds('three');
+              }}
+            >
+              <p className="label">
+                3
+                <br />
+                Rounds
+              </p>
+            </button>
+            <button
+              className={`optionButton ${rounds === 'five' ? 'selected' : ''}`}
+              onClick={() => {
+                setRounds('five');
+              }}
+            >
+              <p className="label">
+                5
+                <br />
+                Rounds
+              </p>
+            </button>
+            <button
+              className={`optionButton ${rounds === 'ten' ? 'selected' : ''}`}
+              onClick={() => {
+                setRounds('ten');
+              }}
+            >
+              <p className="label">
+                10
+                <br />
+                Rounds
+              </p>
+            </button>
+            <button
+              className={`optionButton ${
+                rounds === 'twenty-five' ? 'selected' : ''
+              }`}
+              onClick={() => {
+                setRounds('twenty-five');
+              }}
+            >
+              <p className="label">
+                25
+                <br />
+                Rounds
+              </p>
+            </button>
+          </div>
         </div>
         <Button
           label="Start"
