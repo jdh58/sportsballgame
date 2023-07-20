a3ca1955bd0a3a06e34d7a7969fa3214a5b90959
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-07-20 11:27:01 -0700

    Fully write TSX for leaderboard dropdowns

--- frontend/src/pages/LeaderboardPage.tsx
+++ frontend/src/pages/LeaderboardPage.tsx
@@ -6,9 +6,9 @@ import '../styles/LeaderboardPage.css';
 
 export default function LeaderboardPage() {
   const [sport, setSport] = useState('NBA');
-  const [game, setGame] = useState('whoami');
-  const [mode, setMode] = useState('whoami');
-  const [difficulty, setDifficulty] = useState('whoami');
+  const [game, setGame] = useState('Who Am I?');
+  const [mode, setMode] = useState('3 Rounds');
+  const [difficulty, setDifficulty] = useState('Easy');
 
   const [dropdown, setDropdown] = useState('none');
 
@@ -75,45 +75,192 @@ export default function LeaderboardPage() {
             </div>
             <div>
               <h2>Game</h2>
-              <Selector
-                name="game"
-                options={[
-                  'Who Am I?',
-                  'Higher or Lower',
-                  'Grid',
-                  'Face Mix',
-                  'Call the Call',
-                  'Shot Chart Secret',
-                  'Lineup Decoder',
-                ]}
-                onClick={setGame}
-              />
+              <div className="selectorContainer">
+                <div
+                  className="selector"
+                  onClick={() => {
+                    setDropdown('games');
+                  }}
+                >
+                  {game}
+                </div>
+                <div
+                  className={
+                    dropdown === 'games'
+                      ? 'leaderboardDropdown visible'
+                      : 'leaderboardDropdown'
+                  }
+                >
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setGame('Who Am I?');
+                      setDropdown('none');
+                    }}
+                  >
+                    Who Am I?
+                  </span>
+                  <span
+                    className={'dropdownElement'}
+                    onClick={() => {
+                      setGame('Higher or Lower');
+                      setDropdown('none');
+                    }}
+                  >
+                    Higher or Lower
+                  </span>
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setGame('Grid');
+                      setDropdown('none');
+                    }}
+                  >
+                    Grid
+                  </span>
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setGame('Face Mix');
+                      setDropdown('none');
+                    }}
+                  >
+                    Face Mix
+                  </span>
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setGame('Call the Call');
+                      setDropdown('none');
+                    }}
+                  >
+                    Call the Call
+                  </span>
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setGame('Shot Chart Secret');
+                      setDropdown('none');
+                    }}
+                  >
+                    Shot Chart Secret
+                  </span>
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setGame('Lineup Decoder');
+                      setDropdown('none');
+                    }}
+                  >
+                    Lineup Decoder
+                  </span>
+                </div>
+              </div>
             </div>
             <div>
               <h2>Mode</h2>
-              <Selector
-                name="mode"
-                options={[
-                  'Freeplay',
-                  '3 Rounds',
-                  '5 Rounds',
-                  '10 Rounds',
-                  '25 Rounds',
-                  '3 Minutes',
-                  '5 Minutes',
-                  '10 Minutes',
-                  '25 Minutes',
-                ]}
-                onClick={setMode}
-              />
+              <div className="selectorContainer">
+                <div
+                  className="selector"
+                  onClick={() => {
+                    setDropdown('modes');
+                  }}
+                >
+                  {mode}
+                </div>
+                <div
+                  className={
+                    dropdown === 'modes'
+                      ? 'leaderboardDropdown visible'
+                      : 'leaderboardDropdown'
+                  }
+                >
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setMode('3 Rounds');
+                      setDropdown('none');
+                    }}
+                  >
+                    3 Rounds
+                  </span>
+                  <span
+                    className={'dropdownElement'}
+                    onClick={() => {
+                      setMode('5 Rounds');
+                      setDropdown('none');
+                    }}
+                  >
+                    5 Rounds
+                  </span>
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setMode('10 Rounds');
+                      setDropdown('none');
+                    }}
+                  >
+                    10 Rounds
+                  </span>
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setMode('25 Rounds');
+                      setDropdown('none');
+                    }}
+                  >
+                    25 Rounds
+                  </span>
+                </div>
+              </div>
             </div>
             <div>
               <h2>Difficulty</h2>
-              <Selector
-                name="difficulty"
-                options={['Easy', 'Meduim', 'Hard']}
-                onClick={setDifficulty}
-              />
+              <div className="selectorContainer">
+                <div
+                  className="selector"
+                  onClick={() => {
+                    setDropdown('difficulties');
+                  }}
+                >
+                  {difficulty}
+                </div>
+                <div
+                  className={
+                    dropdown === 'difficulties'
+                      ? 'leaderboardDropdown visible'
+                      : 'leaderboardDropdown'
+                  }
+                >
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setDifficulty('Easy');
+                      setDropdown('none');
+                    }}
+                  >
+                    Easy
+                  </span>
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setDifficulty('Medium');
+                      setDropdown('none');
+                    }}
+                  >
+                    Medium
+                  </span>
+                  <span
+                    className="dropdownElement"
+                    onClick={() => {
+                      setDifficulty('Hard');
+                      setDropdown('none');
+                    }}
+                  >
+                    Hard
+                  </span>
+                </div>
+              </div>
             </div>
           </section>
           <div className="leaderboardContainer">
